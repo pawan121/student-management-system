aef707d8ef0ffa5de96d6f8f97cc9f019fa7b3fd
Rohit Kumar <rohitk8152@gmail.com>
2026-02-18 22:16:10 +0530

    updated code

--- src/main/java/com/app/student/management/controller/StudentController.java
+++ src/main/java/com/app/student/management/controller/StudentController.java
@@ -1,5 +1,4 @@
 package com.app.student.management.controller;
-
 import com.app.student.management.dto.StudentDTO;
 import com.app.student.management.entity.Student;
 import com.app.student.management.repository.StudentRepository;
@@ -8,40 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @CrossOrigin(origins = "*")
 public class StudentController {
 
 	@Autowired
-	private IStudentService student;
-
+	private IStudentService studentService;
 	@Autowired
-	private StudentRepository repo;
-
-	/*@PostMapping("/student/save")
-	public String addStudent(@RequestBody StudentDetailsRequestDTO request) {
-		return student.saveStudentDetails(request);
-
-	}*/
+	private StudentRepository studentRepo;
 
 	@GetMapping("/student")
 	public List<Student> getAllStudent() {
-
-		return repo.findAll();
+		return studentRepo.findAll();
 	}
 
 	@GetMapping("/{sId}")
 	public StudentDTO getStudentById(@PathVariable("sId") String sId) {
-		return student.getStudentDetails(sId);
+		return studentService.getStudentDetails(sId);
 	}
+
 	@GetMapping("/student/count")
 	public Long getNoOfStudentRecords(){
-        return student.getStudentCount();
+        return studentService.getStudentCount();
 	}
 	
 	@DeleteMapping("/student/{sId}")
 	public void deleteStudent(@PathVariable("sId") int sId) {
-	    repo.deleteById(sId);
+	    studentRepo.deleteById(sId);
 	}
 }
